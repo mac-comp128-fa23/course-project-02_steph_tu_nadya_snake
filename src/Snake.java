9b47f8c12bc7b8f3de470c26b5d25b136ef8ae0f
Nadya Konadu <nkonadu@macalester.edu>
2023-11-28 14:26:58 -0600

    Snake Constructor

--- src/Snake.java
+++ src/Snake.java
@@ -1,3 +1,19 @@
+import java.util.LinkedList;
+
+import edu.macalester.graphics.Point;
+
 public class Snake {
-    
+    private LinkedList<Point> snakeBody = new LinkedList<>();
+    private Point tail;
+    private Point head;
+    private int intialLength = 1;
+
+
+    public Snake(Point pos) {
+        this.head = pos;
+        this.tail = pos;
+        this.snakeBody.push(this.head);
+
+    }
+
 }
