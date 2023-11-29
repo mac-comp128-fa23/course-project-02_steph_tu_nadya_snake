a3e8e62f1e989f19a336b7c2defd21b94cf0e7b3
Tran Minh Tu <tranminhtu06052004@gmail.com>
2023-11-29 14:48:52 -0600

    update board

--- src/Board.java
+++ src/Board.java
@@ -1,39 +1,55 @@
+import java.util.Iterator;
+
 public class Board {
+    private Cell[][] cells;
     final int ROW = 20;
     final int COL = 20;
-    private Cell[][] cells;
     private Snake snake;
     private Food food;
+    private int score;
+    public boolean endGame;
 
     public Board() {
-        this.cells = new Cell[this.ROW][this.COL];
-        for (int i = 0; i < this.cells.length; i++) {
-            for (int j = 0; j < this.cells[i].length; j++) {
-                this.cells[i][j] = new Cell(i, j);
-                this.cells[i][j].setType("empty");
+        cells = new Cell[ROW][COL];
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells[i].length; j++) {
+                cells[i][j] = new Cell(i, j);
+                cells[i][j].setType("empty");
             }
         }
 
-        this.snake = new Snake(this.cells[10][10]); //generates the snake in this cell
-        this.food = new Food();
+        Cell snakeHead = new Cell(10, 10);
+        snake = new Snake(snakeHead);
+        food = new Food();
+        endGame = false;
     }
-
-    public void moveSnake() {}
 
     public Cell[][] getCells() {
         return this.cells;
     }
 
     public void eatFood() {
-        //if snake eats current food
-        this.generateFood();
+        //if (snake.getHead().getCol() - food.) {
+            score = score + 1;
+            generateFood();
+        }
+    }
+
+    public void hit() {
+        Cell current = snake.getHead();
+        //while ()
+        endGame = true;
     }
 
     public void generateFood(){
-        int row = (int) (Math.random() * this.ROW);
-        int col = (int) (Math.random() * this.COL);
-        if (this.cells[row][col].getType() != "snake" && this.cells[row][col].getType() != "food") {
-            this.cells[row][col].setType("food");
+        boolean newFood = false;
+        while (!newFood) {
+            int row = (int) (Math.random() * ROW);
+            int col = (int) (Math.random() * COL);
+            if (cells[row][col].getType() != "snake" && cells[row][col].getType() != "food") {
+                cells[row][col].setType("food");
+                newFood = true;
+            }
         }
     }
 }
