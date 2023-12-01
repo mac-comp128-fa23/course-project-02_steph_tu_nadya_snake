5818fa00138696da42cda178734af8f4c813ce72
Stephanie Miles <118240368+stephaniemiless@users.noreply.github.com>
2023-12-01 09:46:57 -0600

    Revert "Merge branch 'main' of https://github.com/mac-comp128-fa23/course-project-02_steph_tu_nadya_snake"
    This reverts commit c3dd0244da4ebbeadf7f6fcec9a93fcf34f922ac, reversing
    changes made to de857b1ab3f7dffec8bb807fc771bc33c199eaed.

--- src/Snake.java
+++ src/Snake.java
@@ -1,40 +1,13 @@
-import java.awt.Color;
 import java.util.LinkedList;
 
-import edu.macalester.graphics.GraphicsGroup;
-
-public class Snake extends GraphicsGroup{
+public class Snake {
     private LinkedList<Cell> snakeBody = new LinkedList<>();
     private Cell head;
-    private Cell[][] cells;
-    
-     
 
-    public Snake(Cell pos, Cell[][] cells) {
+    public Snake(Cell pos) {
         this.head = pos;
         this.snakeBody.push(this.head);
         this.head.setType("snake");
-        this.cells = cells;
-    }
-
-
-    public void draw() {
-        for (Cell cell : this.snakeBody) {
-            cell.setFillColor(Color.GREEN);
-            cell.setFilled(true);
-            this.cells[cell.getRow()][cell.getCol()] = cell;
-        }
-    }
-
-
-    public void move(Cell nextCell)
-    {
-        Cell tail = this.snakeBody.peek();
-        tail.setType("board");
- 
-        this.head = nextCell;
-        this.head.setType("snake");
-        this.snakeBody.push(this.head);
     }
 
     
@@ -55,6 +28,9 @@ public class Snake extends GraphicsGroup{
         }
     }
 
+    public void move(){
+
+    }
 
     public int getTailLength(){
         return this.snakeBody.size() - 1;
