ce02bd2328444e784df9547bcfd0852c53d9e471
Nadya Konadu <nkonadu@macalester.edu>
2023-12-01 08:51:53 -0600

    Added Snake (Start Move())

--- src/Game.java
+++ src/Game.java
@@ -1,30 +1,37 @@
 import java.awt.Color;
 import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.GraphicsGroup;
 
 public class Game {
     private CanvasWindow window;
     private Board board;
 
     public Game() {
-        window = new CanvasWindow("Snake", 600, 600);
+        this.window = new CanvasWindow("Snake", 600, 600);
 
-        board = new Board();
-        Cell[][] cells = board.getCells();
+        this.board = new Board();
+        Cell[][] cells = this.board.getCells();
         for (int i = 0; i < cells.length; i++) {
             for (int j = 0; j < cells[i].length; j++) {
                 cells[i][j].setLocation();
                 if ((i + j) % 2 == 0) {
                     cells[i][j].setFillColor(Color.LIGHT_GRAY);
-                    window.add(cells[i][j]);
+                    this.window.add(cells[i][j]);
                 } else {
                     cells[i][j].setFillColor(Color.GRAY);
-                    window.add(cells[i][j]);
+                    this.window.add(cells[i][j]);
                 }
             }
         }
 
-        window.add(board.getFood());
-    }
+        this.window.add(this.board.getFood());
+        Snake snake = this.board.getSnake();
+        snake.draw(); 
+        this.window.add(snake);
+}
+    
+
+   
 
     public void generateBackground() {
         
